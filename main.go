2a1325a56bc9e4f951024480371cd6d5d5d83a43
Kurban <ggavnoo228@gmail.com>
2025-12-24 23:44:28 +0300

    пока сделать только exit и add

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"Financial_tracker/command"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("push git")
+	var cmd string
+	var money float64
+
+	for {
+		fmt.Scan(&cmd)
+		switch cmd {
+		case "exit":
+			fmt.Println("bye")
+			return
+		case "add":
+			fmt.Print("какая сумма пополнения ")
+			fmt.Scan(&money)
+			if money <= 0 {
+				fmt.Println("ошибка при пополнение")
+				continue
+			}
+			command.Add(money)
+		case "take":
+			fmt.Print("сколько вы хотите вывести? ")
+		}
+	}
 }
