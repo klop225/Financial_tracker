1f5076dd5ef8cac5f09f7fd983f30612ae89a7f2
Kurban <ggavnoo228@gmail.com>
2026-01-13 23:13:48 +0300

    сделал историю операций

--- storage/storage.go
+++ storage/storage.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func ReadBalance(money float64, b bool) {
@@ -12,9 +13,11 @@ func ReadBalance(money float64, b bool) {
 
 	if b {
 		WriteBalance(balance + money)
+		WriteHistory("Пополнил", money, balance+money)
 		return
 	} else if money < balance {
 		WriteBalance(balance - money)
+		WriteHistory("Снял", money, balance-money)
 		return
 	}
 	fmt.Println("столько на баласне нету")
@@ -26,4 +29,14 @@ func WriteBalance(balance float64) {
 	defer file.Close()
 
 	fmt.Fprintf(file, "%.2f", balance)
+}
+
+func WriteHistory(operation string, money, balance float64) {
+	file, _ := os.OpenFile("history", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	defer file.Close()
+
+	time := time.Now().Format("2006-01-02 15:04:05")
+
+	fmt.Fprintf(file, "[%s] %s %.2f баланс сейчас %.2f\n", time, operation, money, balance)
 }
