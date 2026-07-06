2d362232fc6d6fc9453db9d5484d412cb04d879e
Kurban <ggavnoo228@gmail.com>
2026-07-07 02:42:42 +0300

    добавил две функции

--- command/command.go
+++ command/command.go
@@ -9,3 +9,11 @@ func Add(money float64) error {
 func Take(money float64) error {
 	return storage.Withdraw(money)
 }
+
+func GetBalance() (float64, error) {
+	return storage.GetBalance()
+}
+
+func GetHistory() (string, error) {
+	return storage.GetHistory()
+}
