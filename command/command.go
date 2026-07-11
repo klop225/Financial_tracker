3b84bbeff23aa3123261a86108f637d857bd02cd
Kurban <ggavnoo228@gmail.com>
2026-07-11 21:43:48 +0300

    все переделал под новую архиктектуру

--- command/command.go
+++ command/command.go
@@ -2,18 +2,26 @@ package command
 
 import "Financial_tracker/storage"
 
-func Add(money float64) error {
-	return storage.Deposit(money)
+type CommandHadler struct {
+	store *storage.File
 }
 
-func Take(money float64) error {
-	return storage.Withdraw(money)
+func NewCommandHadler(store *storage.File) *CommandHadler {
+	return &CommandHadler{store: store}
 }
 
-func GetBalance() (float64, error) {
-	return storage.GetBalance()
+func (c *CommandHadler) Add(money float64) error {
+	return c.store.Deposit(money)
 }
 
-func GetHistory() (string, error) {
-	return storage.GetHistory()
+func (c *CommandHadler) Take(money float64) error {
+	return c.store.Withdraw(money)
+}
+
+func (c *CommandHadler) GetBalance() (float64, error) {
+	return c.store.GetBalance()
+}
+
+func (c *CommandHadler) GetHistory() (string, error) {
+	return c.store.GetHistory()
 }
