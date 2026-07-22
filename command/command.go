0a2c97429d5cc750e7e8b2112d07b9e44466a3b3
Kurban <ggavnoo228@gmail.com>
2026-07-22 20:46:45 +0300

    добавил интерфейсы для того что бы легко перейти на postgreSQL и исправил синтаксические ошибки

--- command/command.go
+++ command/command.go
@@ -1,27 +1,31 @@
 package command
 
-import "Financial_tracker/storage"
-
-type CommandHadler struct {
-	store *storage.File
+type Storage interface {
+	Deposit(money float64) error
+	Withdraw(money float64) error
+	GetBalance() (float64, error)
+	GetHistory() (string, error)
+}
+type CommandHandler struct {
+	store Storage
 }
 
-func NewCommandHadler(store *storage.File) *CommandHadler {
-	return &CommandHadler{store: store}
+func NewCommandHandler(store Storage) *CommandHandler {
+	return &CommandHandler{store: store}
 }
 
-func (c *CommandHadler) Add(money float64) error {
+func (c *CommandHandler) Add(money float64) error {
 	return c.store.Deposit(money)
 }
 
-func (c *CommandHadler) Take(money float64) error {
+func (c *CommandHandler) Take(money float64) error {
 	return c.store.Withdraw(money)
 }
 
-func (c *CommandHadler) GetBalance() (float64, error) {
+func (c *CommandHandler) GetBalance() (float64, error) {
 	return c.store.GetBalance()
 }
 
-func (c *CommandHadler) GetHistory() (string, error) {
+func (c *CommandHandler) GetHistory() (string, error) {
 	return c.store.GetHistory()
 }
